eb94f8813991cdbbade22b217040132c43a0b708
= <=>
2025-06-18 12:10:11 +0300

    add

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,21 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-cd api
-npm install # Required because npm ci will install only prod dependencies because of app services environment
+cd api || exit 1
+
+echo "📦 Installing dependencies..."
+npm install
+
+echo "🧹 Cleaning build..."
 npm run clean
+
+echo "🔧 Building project..."
 npm run build
 
+echo "🗑️ Removing old content..."
 rm -rf /home/site/wwwroot/*
-cp -r /home/site/repository/api/* /home/site/wwwroot/
+
+echo "📁 Copying necessary files..."
+cp -r dist/* /home/site/wwwroot/
+cp package.json /home/site/wwwroot/
+cp -r node_modules /home/site/wwwroot/
